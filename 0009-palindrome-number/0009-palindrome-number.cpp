bd5ac3eb228cc1334b82f89a4a88e7889dbc5e3d
Bhushan Patil <bpatil.00001@gmail.com>
2026-07-25 20:15:45 +0530

    Time: 7 ms (13.7%), Space: 8.5 MB (65.75%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int xCopy = x;
+        long long rev = 0;
+        while (x > 0) {
+            int rem = x % 10;
+            rev = rev * 10 + rem;
+            x /= 10;
+        }
+        return rev == xCopy;
+    }
+};
